117bb66baf974e33b9750e4024d9617c18bf3cce
JeskewNKC <32969486+JeskewNKC@users.noreply.github.com>
2018-09-07 07:52:37 -0500

    Add files via upload

--- ICP3 Part2.py
+++ ICP3 Part2.py
@@ -1,6 +1,7 @@
 import csv
 inputfile = "codon.tsv"
 f = open(inputfile, "r")
+
 genetic_code = {}
 for row in f:
         columns = row.split()
@@ -9,7 +10,7 @@ for row in f:
 seq = input('Enter a sequence: ')
 seq = seq.upper()
 seq = seq.replace(" ", "")
-# test to see if sequence is divisible by 3 if not ignore last nucleotides until %3 == 0
+# test to see if sequence is divisible by 3 if not ignore last nucleotides until %3 == 0... up to 2 nucleotides trimmed
 
 if len(seq)%3 != 0:
     seq = seq[:-1]
@@ -18,6 +19,7 @@ if len(seq)%3 !=0:
 
 # below is list comprehension expression to create a list of 3 nucleotide codons
 codons = [seq[i:i+3] for i in range(0, len(seq), 3)]
+print(codons)
 
 # Protein translation and appending a.a. to new list [protein_list]
 protein_list = []
@@ -25,8 +27,9 @@ for aa in codons:
     protein = ([genetic_code[aa]])
     protein_list.append(*protein)
 
-print(protein_list)
+# print(protein_list)
 
 # Used list comprehension to create a.a. and a.a count dictionary
 d = dict( [(i, protein_list.count(i)) for i in set(protein_list) ])
 print (d)
+
